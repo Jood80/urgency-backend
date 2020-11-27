6279fa3b69849cdf9afcc1527876fe760a1273f1
Nujood <shawar.nujood@gmail.com>
2020-11-27 20:59:55 +0200

    Fetching all news data from backend #5

--- server/app.js
+++ server/app.js
@@ -8,8 +8,5 @@ app.use(express.urlencoded({ extended: false }))
 
 
 app.use(router)
-app.get('*', (req, res) => {
-  res.send('hello')
-})
 
 module.exports = app
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router();
+
+const getAllNewData = require('./getAllNewsData')
 const { Error404, Error500 } = require('../helpers');
+
+router.get('/news', getAllNewData)
 
 router.use(Error404);
 router.use(Error500);
 
-module.exports = router;
+module.exports = router
