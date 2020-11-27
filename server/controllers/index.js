81a1bcc29ee7ffdb525e6797bf4fdda663fb6fd4
Nujood <shawar.nujood@gmail.com>
2020-11-27 19:05:49 +0200

    add errors handler #3

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const { Error404, Error500 } = require('../helpers');
+
+router.use(Error404);
+router.use(Error500);
 
 module.exports = router;
