3bbec10fbab4471a076402674e67290d34d77b4a
Nujood <shawar.nujood@gmail.com>
2020-11-28 10:15:55 +0200

    change testing, news endpoints into /api/xxx

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const getAllNewData = require('./getAllNewsData')
 const { Error404, Error500 } = require('../helpers');
 
-router.get('/news', getAllNewData)
-router.get('/testing', (req, res) => {
+router.get('/api/news', getAllNewData)
+router.get('/api/testing', (req, res) => {
 
   res.send('Testing page content')
 })
