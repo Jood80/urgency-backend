0a173bef54fb97dc8b67391f746bd6cdbedd8199
Nujood <shawar.nujood@gmail.com>
2020-11-28 21:41:04 +0200

    make an initial validation for signUp form #14

--- server/controllers/postUsers.js
+++ server/controllers/postUsers.js
@@ -1,15 +1,19 @@
 const postUserQuery = require("../database/queries/postUsers")
+const {checkValidation} = require('../helpers')
 
 module.exports = (req, res) => {
-  const {userName, password, email} = req.body
 
-  postUserQuery({ userName, password, email })
-    .then(response => {
-      console.log(response);
-      if (response.rowCount === 1) return res.status(200).json({ message: 'user has been created successfully' })
-    })
-    .catch(err => res
-      .status(400)
-      .json({ message: 'An error occured! Make sure you filled all the fields.' })
-    ) 
+  checkValidation(req.body)
+    .then(result => {
+      console.log("res", result);
+      postUserQuery(result)
+        .then(response => {
+          console.log(response);
+          if (response.rowCount === 1) return res.status(200).json({ message: 'user has been created successfully' })
+        })
+        .catch(err => res
+          .status(400)
+          .json({ message: 'An error occured! Make sure you filled all the fields.' })
+        )
+    }) 
 }
--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,3 +1,4 @@
+const {checkValidation} = require('./validation')
 const { Error404, Error500 } = require('./errors');
 
-module.exports = { Error404, Error500 };
+module.exports = { Error404, Error500 , checkValidation};
