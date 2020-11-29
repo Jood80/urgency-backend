fd407b09311304d59f0133181e8085b017988dc5
Nujood <shawar.nujood@gmail.com>
2020-11-29 12:26:15 +0200

    update the api requested data

--- server/controllers/getAllNewsData.js
+++ server/controllers/getAllNewsData.js
@@ -3,12 +3,17 @@ require('env2')('./config.env');
 const newsApi = new NewsAPI(`${process.env.API_KEY}`);
 
 module.exports = (req, res) => {
-  newsApi.v2.sources({
-    category: 'technology',
-    language: 'en',
-    country: 'us'
-  }).then(response => {
-     res.send({
+ newsApi.v2.everything({
+  q: 'technology',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2020-11-10',
+  to: '2020-11-20',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 2
+}).then(response => {
+      res.send({
       response,
       seo: {
         description: 'News',
