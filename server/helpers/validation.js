f60ec721f5a72a3ae2bb3539b69a1629c60d29a0
Nujood <shawar.nujood@gmail.com>
2020-11-29 08:39:01 +0200

    update validation schema

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -1,21 +1,17 @@
 const Yup = require('yup');
 
 exports.checkValidation = (body) => {
-  console.log("body", body);
   const schema = Yup.object().shape({
     userName: Yup.string().min(3).required('Name is required'),
-    password: Yup.string()
-    .alphanum()
-    .min(6)
-    .max(255)
-    .required('Password is required'),
+    password: Yup.string().min(3).required('Password is required'),
     email: Yup.string()
       .email('Invalid email')
       .required('Email is required'),
   });
 
   return schema.validate(body).catch((err) => {
-    err.name;
-    err.errors;
+    err.userName;
+    err.password;
+    err.email;
   });
 };
